e401fab6c597db346f12b147062831db0fb08129
Jerry Owusu <65233860+jerryowusu@users.noreply.github.com>
2022-04-11 10:50:40 +0000

    Get current date

--- src/services/FetchData.js
+++ src/services/FetchData.js
@@ -7,3 +7,15 @@ const api = axios.create({
   },
 });
 
+const today = new Date();
+const month = today.getMonth() + 1;
+const year = today.getFullYear();
+const day = today.getDate() - 1;
+
+const currentDate = `${year}-${month}-${day}`;
+
+// const currentDate = `${today.getFullYear()}-${(`0${today.getMonth() + 1}`).slice(-2)}-${(`0${today.getDate() - 1}`).slice(-2)}`;
+
+const getData = () => api.get(`api/${currentDate}`);
+
+export default getData;
