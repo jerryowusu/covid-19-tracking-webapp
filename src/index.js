9c59096a3812aeeafc3c89581316bf676416deae
Jerry Owusu <65233860+jerryowusu@users.noreply.github.com>
2022-04-11 11:24:48 +0000

    Update store

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-// import { Provider } from 'react-redux';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
-// import reportWebVitals from './reportWebVitals';
+import store from './redux/configureStore';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
-  // <Provider>
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  // </Provider>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
 );
