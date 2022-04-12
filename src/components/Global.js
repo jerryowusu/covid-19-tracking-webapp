f133708265f2d16c9d442769b2236b193d5ca2b3
Jerry Owusu <65233860+jerryowusu@users.noreply.github.com>
2022-04-12 13:37:44 +0000

    update global component

--- src/components/Global.js
+++ src/components/Global.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+// import { Link } from 'react-router-dom';
+import { getCovidData } from '../redux/covidData';
 
-const Global = () => (
-  <div>Home</div>
-);
+const Global = () => {
+  const data = useSelector((store) => store.data);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getCovidData());
+  }, []);
+
+  return (
+    <div className="container">
+      <h2>
+        kwame
+        {data}
+      </h2>
+    </div>
+  );
+};
 
 export default Global;
